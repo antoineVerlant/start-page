fc4d941eff1d9b7a3aa13d964ca097a3d97dd0fb
Babwin <i3abwin@gmail.com>
2015-03-26 10:18:30 +0100

    Update main.js
    Add shortcut with key 1,2,3,... for each site

--- js/main.js
+++ js/main.js
@@ -7,14 +7,7 @@ var config =
         { "url": "https://boards.4chan.org/b/",                           	        "image": "images/4chan_logo.png" },
         { "url": "http://ecampus.ipl.be",                                           "image": "images/logo_ecampus.png"  },
     ],
-    "search":
-    {
-        "url"  : "https://duckduckgo.com",
-        "name" : "DuckDuckGo",
-        "query": "q"
-    }
 }, d = document;
-
 d.addEventListener("DOMContentLoaded", function()
 {
     d.removeEventListener("DOMContentLoaded", arguments.callee, false);
@@ -23,7 +16,7 @@ d.addEventListener("DOMContentLoaded", function()
     {
 		var image = new Image();
 		image.src = config.sites[i].image;
-		image.height = 100;
+		image.height = 50;
         var p = i / 12,
 			site = d.createElement("a");
         site.href = config.sites[i].url;
@@ -35,22 +28,22 @@ d.addEventListener("DOMContentLoaded", function()
             page.id = "page" + p;
 
             d.querySelector("#sites").appendChild(page);
-            // d.querySelector("#navigation ul").appendChild((d.createElement("li")));
 
             if (p === 0)
             {
-                // d.querySelector("#navigation ul li").classList.add("selected");
                 d.querySelector("#sites > div").classList.add("selected");
             }
         }
 
         d.querySelector("#page" + Math.floor(p)).appendChild(site);
     }
-
-    var form = d.querySelector("#search form"),
-        text = form.querySelector("input");
-    form.action      = config.search.url;
-    text.name        = config.search.query;
-    text.placeholder = config.search.name;
-    text.focus();
 });
+	var isCtrl = false;
+	document.onkeyup=function(e) {
+		for (var i = 0, MAX = config.sites.length; i < MAX; ++i){
+			if(e.which == 49+i){
+				isCtrl=false;
+				document.location.href=config.sites[i].url;
+			}
+		}
+	}
